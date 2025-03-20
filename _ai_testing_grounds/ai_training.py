475f2eb4049d7c990338f2655a900fa7c64271c8
AX028 <ax02846@gmail.com>
2025-03-19 19:17:22 -0500

    Delete _ai_testing_grounds directory
    Signed-off-by: AX028 <ax02846@gmail.com>

--- _ai_testing_grounds/ai_training.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import multiprocessing as mp
-from _ai.enemy_ai import AI
-from _characters import *
-from _enemies import *
-from _armor import *
-from _logic import *
-from _weapons import *
-
